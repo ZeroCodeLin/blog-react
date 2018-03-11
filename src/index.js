1699460a95673b42e5d3b41f81d1cb26d835bcef
lemon <540846207@qq.com>
2018-03-11 23:24:49 +0800

    style: 修改样式

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React,{Component} from "react"
 import ReactDOM from 'react-dom'
-
 import Router from './router'
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux';
 import example from './reducers/example'
+import style from './index.less'
 // 安装redux-devtools-extension的可视化工具。
 import { composeWithDevTools } from 'redux-devtools-extension'
 const reducer = combineReducers({ example })
